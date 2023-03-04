efeacb6e8019ffeb80579ea091045d635c311236
Nicolò Sponziello <nicolosponziello@gmail.com>
2023-03-04 19:58:06 +0100

    Aggiungere i file di progetto.

---
+++ WorkoutTimer/WorkoutTimer/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.CommunityToolkit;
+
+namespace WorkoutTimer
+{
+    public partial class MainPage : ContentPage
+    {
+        private WorkoutTimerViewModel _viewModel;
+        public MainPage()
+        {
+            InitializeComponent();
+            _viewModel = new WorkoutTimerViewModel();
+            BindingContext = _viewModel;
+        }
+    }
+}
---
+++ WorkoutTimer/WorkoutTimer/WorkoutTimerViewModel.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace WorkoutTimer
+{
+    public class WorkoutTimerViewModel : INotifyPropertyChanged
+    {
+        private int _sets;
+        private int _elapsedTime;
+        private Color _bgColor;
+        public int Sets 
+        {
+            get => _sets;
+            private set
+            {
+                _sets = value;
+                OnPropertyChanged(nameof(Sets));
+            }
+        }
+
+        private bool _isTimerRunning;
+
+        public Boolean IsTimerRunning
+        {
+            get => _isTimerRunning;
+            private set
+            {
+                _isTimerRunning = value;
+                OnPropertyChanged(nameof(IsTimerRunning));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int ElapsedTime 
+        {
+            get => _elapsedTime;
+            private set
+            {
+                if (value < 0)
+                {
+                    _elapsedTime = 0;
+                }
+                else
+                {
+                    _elapsedTime = value;
+                }
+                OnPropertyChanged(nameof(ElapsedTime));
+            }
+        }
+
+        public Command<int> SetRestTimeCommand { get; private set; }
+
+        public Command IncreaseSetsCommand { get; private set; }
+
+        public Command DecreaseSetsCommand { get; private set; }
+
+        public Command ResetSetsCommand { get; private set; }
+
+        public Command StartTimerCommand { get; private set; }
+
+        public Command StopTimerCommand { get; private set; }
+
+        public Color BackgroundColor 
+        {
+            get => _bgColor;
+            private set
+            {
+                _bgColor = value;
+                OnPropertyChanged(nameof(BackgroundColor));
+            }
+        }
+
+
+        public WorkoutTimerViewModel() 
+        {
+            BackgroundColor = Color.Green;
+            Sets = 0;
+            _isTimerRunning = false;
+            ElapsedTime = 0;
+            SetRestTimeCommand = new Command<int>(SetRest);
+            StartTimerCommand = new Command(StartTimer);
+            IncreaseSetsCommand = new Command(IncreaseSets);
+            DecreaseSetsCommand = new Command(DecreaseSets);
+            ResetSetsCommand = new Command(ResetSets);
+            StopTimerCommand = new Command(StopTimer);
+        }
+
+        private void ResetSets()
+        {
+            Sets = 0;
+            Vibration.Vibrate(TimeSpan.FromMilliseconds(100));
+        }
+
+        private void IncreaseSets()
+        {
+            Sets++;
+        }
+
+        private void DecreaseSets()
+        {
+            Sets--;
+        }
+
+        private void SetRest(int rest)
+        {
+            if (_isTimerRunning)
+            {
+                ElapsedTime += rest;
+            }
+            else
+            {
+                ElapsedTime = rest;
+            }
+        }
+
+        private void StartTimer()
+        {
+            _isTimerRunning = true;
+            BackgroundColor = Color.Red;
+            Device.StartTimer(new TimeSpan(0, 0, 1), () =>
+            {
+                ElapsedTime--;
+                if (ElapsedTime <= 0)
+                {
+                    _isTimerRunning = false;
+                    BackgroundColor = Color.Green;
+                    Vibration.Vibrate(TimeSpan.FromSeconds(1));
+                }
+                return ElapsedTime > 0;
+            });
+        }
+
+        private void StopTimer()
+        {
+            ElapsedTime = 0;
+            _isTimerRunning = false;
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
